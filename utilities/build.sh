aaa91bdff02292f3e271bc9b7646118a8c85717c
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2017-08-22 10:35:49 -0400

    Include some things inferred by circle

--- utilities/build.sh
+++ utilities/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+mkdir -p ~/.go_project/src/github.com/${CIRCLE_PROJECT_USERNAME}
+ln -s ${HOME}/${CIRCLE_PROJECT_REPONAME} ${HOME}/.go_project/src/github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
+go get -t -d -v ./...
+
 cd wrapper
-go get
-go build
+go build -v
 
 cd ../releaser
-go get
-go build
+go build -v
 cd test
 make
 
