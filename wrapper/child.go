fb309f230f2ed5d19b36d0b445dad5e28ec5ab11
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2017-08-30 13:18:37 -0400

    Fix wrapper's stderr and stdout passthroughs

--- wrapper/child.go
+++ wrapper/child.go
@@ -1,39 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
+	"os"
 	"os/exec"
 )
 
 // Run a command and report when it exits.
 func run(cmd *exec.Cmd, done chan struct{}) {
-	// Set up pipes so we can see the command's text output.
-	stdout, err := cmd.StdoutPipe()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Start()
 	check(err)
-
-	stderr, err := cmd.StderrPipe()
-	check(err)
-
-	thru := func(f io.ReadCloser) {
-		s := bufio.NewScanner(f)
-
-		// TODO: Route stdout and stderr to channels so they can be
-		// accumulated in a profile.
-
-		for s.Scan() {
-			fmt.Println(s.Text())
-		}
-	}
-
-	err = cmd.Start()
-	check(err)
-
-	go thru(stderr)
-	go thru(stdout)
-
 	err = cmd.Wait()
+	fmt.Println(err)
 
 	// There is a race condition between child exit and socket EOF; we don't
 	// know which will happen first.  Nevertheless, if the child exits,
