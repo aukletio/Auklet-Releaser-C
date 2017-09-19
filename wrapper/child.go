56071a98cdce102f1a20502331f72c0a52473f33
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2017-09-19 10:21:45 -0400

    Improve wrapper: JSON tag, checksum, waitgroups
    Fix GitTopLevel JSON tag
    
    The wrapper now adds a SHA512 checksum field to each JSON profile.
    
    Use waitgroups in wrapper

--- wrapper/child.go
+++ wrapper/child.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 )
 
 // Run a command and report when it exits.
-func run(cmd *exec.Cmd, done chan struct{}) {
+func run(cmd *exec.Cmd, wg sync.WaitGroup) {
+	defer wg.Done()
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -15,10 +18,4 @@ func run(cmd *exec.Cmd, done chan struct{}) {
 	check(err)
 	err = cmd.Wait()
 	fmt.Println(err)
-
-	// There is a race condition between child exit and socket EOF; we don't
-	// know which will happen first.  Nevertheless, if the child exits,
-	// there is no longer any stdout or stderr to print out (those files
-	// should close). run() should report the exit status of the command.
-	done <- struct{}{}
 }
