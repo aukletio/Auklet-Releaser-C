1e4396e9cd2f4b98236fb5faac148b5fe27fa662
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2017-11-21 13:10:45 -0500

    Fail deploy fast

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+set -e
+VERSION="$(cat VERSION)"
+LIBTAR="libauklet-$VERSION.tgz"
 
 echo 'Compiling profiler...'
 make
+
 echo 'Packaging profiler...'
-VERSION="$(cat VERSION)"
-LIBTAR="libauklet-$VERSION.tgz"
 tar czv -f $LIBTAR libauklet.a
-echo 'Uploading profiler to S3...'
+
+echo 'Installing AWS CLI...'
 sudo apt-get -y install awscli
+
+echo 'Uploading profiler to S3...'
 aws s3 cp $LIBTAR s3://auklet-profiler/$LIBTAR
 aws s3 cp $GOPATH/bin/wrap s3://auklet-profiler/wrap-$VERSION
 aws s3 cp $GOPATH/bin/release s3://auklet-profiler/release-$VERSION
