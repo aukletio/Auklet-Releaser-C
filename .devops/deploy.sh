06ad8f0f03bfd22c13048b01f04061629691fff5
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2017-11-27 11:32:58 -0500

    Fix GOROOT permissions

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -13,19 +13,26 @@ echo 'Compiling wrapper and releaser...'
 mkdir deploy
 export GOFLAGS="-ldflags \"-X main.Version=$VERSION -X main.BuildDate=$TIMESTAMP\""
 echo 'Releaser: linux/amd64'
-GOOS=linux GOARCH=amd64 make releaser && cp $GOPATH/bin/release deploy/release-$VERSION-linux-amd64
+GOOS=linux GOARCH=amd64 make releaser
+cp $GOPATH/bin/release deploy/release-$VERSION-linux-amd64
 echo 'Releaser: windows/amd64'
-GOOS=windows GOARCH=amd64 make releaser && cp $GOPATH/bin/release deploy/release-$VERSION-windows-amd64.exe
+GOOS=windows GOARCH=amd64 make releaser
+cp $GOPATH/bin/release deploy/release-$VERSION-windows-amd64.exe
 echo 'Wrapper: linux/amd64'
-GOOS=linux GOARCH=amd64 make wrapper && cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-amd64
+GOOS=linux GOARCH=amd64 make wrapper
+cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-amd64
 echo 'Wrapper: linux/arm'
-GOOS=linux GOARCH=arm make wrapper && cp $GOPATH/bin/release deploy/wrap-linux-arm
+GOOS=linux GOARCH=arm make wrapper
+cp $GOPATH/bin/release deploy/wrap-linux-arm
 echo 'Wrapper: linux/arm64'
-GOOS=linux GOARCH=arm64 make wrapper && cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-arm64
+GOOS=linux GOARCH=arm64 make wrapper
+cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-arm64
 echo 'Wrapper: linux/mips64'
-GOOS=linux GOARCH=mips64 make wrapper && cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-mips64
+GOOS=linux GOARCH=mips64 make wrapper
+cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-mips64
 echo 'Wrapper: linux/mips64le'
-GOOS=linux GOARCH=mips64le make wrapper && cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-mips64le
+GOOS=linux GOARCH=mips64le make wrapper
+cp $GOPATH/bin/release deploy/wrap-$VERSION-linux-mips64le
 
 echo 'Compiling/packaging profiler...'
 make libauklet.a
