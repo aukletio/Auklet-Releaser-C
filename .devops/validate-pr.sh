139a9d431644859f8774b74172172f3515bfe1c2
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-02-16 14:36:36 -0500

    Improve local build step control

--- .devops/validate-pr.sh
+++ .devops/validate-pr.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [[ -f ~/.localCircleBuild ]]; then
-  echo 'This is a local CircleCI build; no PR to validate.'
-else
-  echo 'Initializing...'
-  cd ~ # Prevents codebase contamination.
-  rm -rf node_modules prnum.txt
-  npm install --no-spin request request-promise > /dev/null 2>&1
-  node $THIS_DIR/validatePr.js
-fi
+echo 'Initializing...'
+cd ~ # Prevents codebase contamination.
+rm -rf node_modules prnum.txt
+npm install --no-spin request request-promise > /dev/null 2>&1
+node $THIS_DIR/validatePr.js
